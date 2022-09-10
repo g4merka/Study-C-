1763e935a8818c8e4b7064049fa1704f62ad4a8f
Yura Diakonov <40857098+g4merka@users.noreply.github.com>
2022-09-10 05:10:55 +0000

    Добавил новые переменные

--- main.cs
+++ main.cs
@@ -2,6 +2,14 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+        int i = 1;
+        double d = 1.2;
+        char c = 'c';
+        bool t = true;
+        bool f = false;
+        decimal d = 0.1M; //указывается, что это число не double
+        float f = 0.2F; //то же самое
+      
+      
   }
 }
