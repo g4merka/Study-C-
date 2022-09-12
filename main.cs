4d364a3ae40f9e2066ce89214a0d5258f190f8c2
Yura @g4merka <yuradiakonov54@outlook.com>
2022-09-12 09:31:32 +0500

    Цикл while записывает все 5 строк, что вводит пользователь и потом выводит их в конце.

--- main.cs
+++ main.cs
@@ -4,14 +4,16 @@ using System.Collections.Generic;
 class Program {
   public static void Main (string[] args) 
   {
-    var list = new List<int>();
-    for(var i = 0; i <= 10; i+=2)
+    var list = new List<string>();
+    while (list.Count < 5)
         {
-            list.Add(i);
+            list.Add(Console.ReadLine());
         }
-    for(var i = 0; i < list.Count; i++)
+    var i = 0;
+    while (i < list.Count)
         {
             Console.WriteLine(list[i]);
+            i++;
         }
     Console.ReadLine();
   }
